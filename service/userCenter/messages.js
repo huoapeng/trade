e62d3ff4672653e0ecc763f73f918d8a59651b75
kent <huoapeng@hotmail.com>
2018-06-04 03:24:42 +0800

    初步完成购买作品消息列表

--- service/userCenter/messages.js
+++ service/userCenter/messages.js
@@ -1,10 +1,50 @@
-var httpUtil=require('../../utils/http.js')
-var async= require('async');
-var appConfig=require('../../appConfig.js');
+var httpUtil = require('../../utils/http.js')
+var async = require('async');
+var appConfig = require('../../appConfig.js');
 var URL = require('url');
-exports.showHtml = function (req, res, next) {     
-	var resultData="";
-	res.render('userCenter/messages',{'results':resultData}) 	 
- 
-}; 
- 
+
+exports.showHtml = function (req, res, next) {
+	if (!req.session.user) {
+		return res.redirect('/login');
+	}
+	var resultData = {};
+	async.waterfall([
+		function (done) {
+			var options = {
+				"path": "/workmessagelist?b=" + req.session.user.id
+			}
+			httpUtil.get(options, function (result, err) {
+				if (err) {
+					done(err, null);
+				} else {
+					for (var i in result['data']) {
+						result['data'][i]['publishDate'] = dateType(result['data'][i]['publishDate'])
+					}
+					resultData['buyerMessageList'] = result;
+					done(null, result);
+				}
+			})
+		},function (onearg, done) {
+			var options = {
+				"path": "/workmessagelist?s=" + req.session.user.id
+			}
+			httpUtil.get(options, function (result, err) {
+				if (err) {
+					done(err, null);
+				} else {
+					for (var i in result['data']) {
+						result['data'][i]['publishDate'] = dateType(result['data'][i]['publishDate'])
+					}
+					resultData['sellerMessageList'] = result;
+					done(null, result);
+				}
+			})
+		}
+	], function (err, results) {
+		res.render('userCenter/messages', { 'results': resultData })
+	});
+};
+function dateType(date) {
+	var date = new Date(date);
+	return date.toLocaleDateString();
+}
